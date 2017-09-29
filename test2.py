3396d3b44731a4b5eff73ef5814ffb95f8e6228d
Andrew Jowers <andrewj@Selene1.FSRoot.com>
2017-09-29 16:51:04 -0400

    started on gui with appJar

--- test2.py
+++ test2.py
@@ -1,14 +1,44 @@
-class PodEpisode():
-    def __init__(self):
-        self.pod_name = None
-        self.episode_name = None
-        self.episode_title = None
-        self.episode_description = None
-        self.episode_url = None
-        self.episode_published = None
-        self.playback_speed = None
-    
+## import the library
+#from appJar import gui
 
-pod = PodEpisode()
-pod.pod_name = 'Pod Name'
-print(pod.pod_name)
+## handle button events
+#def press(button):
+#    if button == "Cancel":
+#        app.stop()
+#    else:
+#        app2 = app.startSubWindow('Demo')
+#        app.addNamedButton("CLOSE", "Demo", app.hideSubWindow)
+#        print(app2.title)
+#        app.showSubWindow('Demo')
+
+## create a GUI variable called app
+#app = gui("autopod")
+#app.addButton('Goto app2',press)
+#tools = ['SEARCH PODS','REMOVE POD','DISPLAY PODS']
+#def tbfuncs(tb):
+#    print(tb)
+#
+#app.addToolbar(tools, tbfuncs,findIcon=True)
+#app.go()
+
+from appJar import gui 
+
+def launch(win):
+    app.showSubWindow(win)
+
+app=gui()
+
+# this is a pop-up
+app.startSubWindow("one", modal=True)
+app.addLabel("l1", "SubWindow One")
+app.stopSubWindow()
+
+# this is another pop-up
+app.startSubWindow("two")
+app.addLabel("l2", "SubWindow Two")
+app.stopSubWindow()
+
+# these go in the main window
+app.addButtons(["one", "two"], launch)
+
+app.go()
